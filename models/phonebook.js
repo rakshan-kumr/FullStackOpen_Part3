dbfab04539741dc57572d3277a3966c8b6d00770
rakshan-kumr <rkshnkumr@gmail.com>
2023-02-05 15:49:15 +0530

    3.21 Deploying the database backend to production

--- models/phonebook.js
+++ models/phonebook.js
@@ -18,7 +18,16 @@ mongoose
 const phonebookSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, minLength: 3, unique: true },
-    number: String,
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function (v) {
+          return /^\d{2,3}-\d{6,}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
   },
   {
     collection: "people",
